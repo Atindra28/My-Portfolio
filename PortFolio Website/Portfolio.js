51484e6a9f65e2203c4127b50ed11ef321228428
Atindra Dubey <atindradubey2810@gmail.com>
2025-10-19 16:19:39 +0530

    Add files via upload

---
+++ PortFolio Website/Portfolio.js
@@ -0,0 +1,22 @@
+const text = ["Designer", "Developer", "Freelancer"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === text.length) {
+    count = 0;
+  }
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById('typing').textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1500); // Pause between words
+  } else {
+    setTimeout(type, 150); // Typing speed
+  }
+})();
